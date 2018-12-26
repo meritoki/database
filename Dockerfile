e03df63a1a2f96b5641f5b533a61a125438674b9
Joaquin Osvaldo Rodriguez <meritoki@yahoo.com>
2018-12-25 21:21:38 -0300

    Docker support

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,1 @@
-FROM mysql/mysql-server
-WORKDIR /meritoki/dailybread/database
+FROM mysql/mysql-server:latest
---
+++ run.sh
@@ -0,0 +1,40 @@
+StartMySQL ()
+{
+    /usr/bin/mysqld_safe ${EXTRA_OPTS} > /dev/null 2>&1 &
+    # Time out in 1 minute
+    LOOP_LIMIT=60
+    for (( i=0 ; ; i++ )); do
+        if [ ${i} -eq ${LOOP_LIMIT} ]; then
+            echo "Time out. Error log is shown as below:"
+            tail -n 100 ${LOG}
+            exit 1
+        fi
+        echo "=> Waiting for confirmation of MySQL service startup, trying ${i}/${LOOP_LIMIT} ..."
+        sleep 1
+        mysql -uroot -e "status" > /dev/null 2>&1 && break
+    done
+}
+CreateMySQLUser()
+{
+    if [ "$MYSQL_PASS" = "**Random**" ]; then
+        unset MYSQL_PASS
+    fi
+
+    PASS=${MYSQL_PASS:-$(pwgen -s 12 1)}
+    _word=$( [ ${MYSQL_PASS} ] && echo "preset" || echo "random" )
+    echo "=> Creating MySQL user ${MYSQL_USER} with ${_word} password"
+
+    mysql -uroot -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '$PASS'"
+    mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION"
+    echo "=> Done!"
+    echo "========================================================================"
+    echo "You can now connect to this MySQL Server using:"
+    echo ""
+    echo "    mysql -u$MYSQL_USER -p$PASS -h<host> -P<port>"
+    echo ""
+    echo "Please remember to change the above password as soon as possible!"
+    echo "MySQL user 'root' has no password but only allows local connections"
+    echo "========================================================================"
+}
+StartMySQL
+CreateMySQLUser
